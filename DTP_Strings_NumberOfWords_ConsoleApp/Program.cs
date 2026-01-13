9fa502cace9277d63d26d1dd5bd02d8fed025555
Tobias R. <no@thankyou.com>
2026-01-13 11:29:18 +0100

    Added project for "Number of words" Added project for "Revert words order"

---
+++ DTP_Strings_NumberOfWords_ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DTP_Strings_NumberOfWords_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "This is a test";
+            Console.WriteLine(GetWordCount(str));
+        }
+        static int GetWordCount(string str)
+        {
+            string[] words = str.Split(" ");
+            return words.Length;
+            
+        }
+    }
+}
---
+++ DTP_Strings_RevertWordsOrder_ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DTP_Strings_RevertWordsOrder_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "A, B. C";
+            Console.WriteLine(ReverseWordOrder(str));
+        }
+        static string ReverseWordOrder(string str)
+        {
+            string newString = "";
+            string[] newStringArray = str.Split(" ");
+            for (int i = newStringArray.Length - 1; i >= 0; i--)
+            {
+                newString += newStringArray[i] + " ";               
+            }
+            if (str.Contains(".") && str.LastIndexOf(".") == str.Length-1)
+                {
+                    newString = newString.Replace(".", "");
+                    newString = newString.Insert(str.Length-1, ".");
+                    return newString;
+                }
+            return newString;
+
+        }
+    }
+}
