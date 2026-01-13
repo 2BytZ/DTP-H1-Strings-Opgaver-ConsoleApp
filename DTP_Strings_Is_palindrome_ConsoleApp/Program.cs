a14f2d0e02f1c71b171fa1693d36cf1b821947fa
Tobias R. <no@thankyou.com>
2026-01-13 09:34:04 +0100

    Added project for "Is Palindrome"

---
+++ DTP_Strings_Is_palindrome_ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DTP_Strings_Is_palindrome_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "racecar";
+            Console.WriteLine(Method(str));
+        }
+        static bool Method(string str)
+        {
+            string forwards = str;
+            string backwards = "";
+            for (int i = forwards.Length; i <= 0; i--)
+                backwards += forwards[i];
+            if (String.Compare(forwards, backwards) == 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
