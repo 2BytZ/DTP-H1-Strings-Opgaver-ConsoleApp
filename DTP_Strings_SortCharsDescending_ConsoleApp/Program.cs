8fbda5d129bb71062da9d073e17d5f8af490e3ec
Tobias R. <no@thankyou.com>
2026-01-13 12:46:20 +0100

    Added project for "how many occurances" solution

---
+++ DTP_Strings_SortCharsDescending_ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DTP_Strings_How_Many_Occurances_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "do it now";
+            string substr = "do"; //not a ""substring"" but serves same purpose
+
+            Console.WriteLine(NumberOfOccurances(str,substr));
+        }
+        static int NumberOfOccurances(string str, string substr)
+        {
+            int i = 0;
+            string[] stringArray = str.Split();
+            if (stringArray.Contains(substr))
+            {
+                foreach (var item in stringArray)
+                {
+                    if (item.Contains(substr))
+                    {
+                        i++;
+                    }
+                }
+            }
+            return i;
+
+        }
+    }
+}
