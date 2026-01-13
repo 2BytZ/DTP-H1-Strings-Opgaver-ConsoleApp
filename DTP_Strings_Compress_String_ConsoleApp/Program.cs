103326f09d646a75bb8e44b830cb189487f84a75
Tobias R. <no@thankyou.com>
2026-01-13 14:23:17 +0100

    Added the final 2 projects; "Compressed string" and "Sort characters descending"

---
+++ DTP_Strings_Compress_String_ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace DTP_Strings_Compress_String_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "kkkktttrrrrrrrrrr";
+            if (String.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("String Empty");
+            }
+            Console.WriteLine(CompressString(str));
+        }
+        static string CompressString(string str)
+        {
+            string result = "";
+            var strList = str.ToArray();
+            int j = 0;
+            int i = 0;
+            for (i = 0; i <= strList.Length - 2; i++)
+            {
+
+                if (strList[i] == strList[i + 1])
+                {
+                    j++;
+                }
+                else
+                {
+                    j++;
+                    result += strList[i] + $"{j}";
+                    j = 0; // reset counter
+                }
+            }
+            j++;
+            result += strList[i] + $"{j}";
+            return result;
+        }
+    }
+}
---
+++ DTP_Strings_SortCharsDescending_ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.ComponentModel;
+
+namespace DTP_Strings_SortCharsDescending_ConsoleApp
+{
+    internal class Program
+    { //char c = (char)65;
+        static void Main(string[] args)
+        {
+            string str = "fohjwf42os";
+            Console.WriteLine(SortChars_Descending(str));
+        }
+        static string SortChars_Descending(string str)
+        {
+            string result = "";
+            var charList = (str.ToCharArray());
+            //Sort charList to descending order
+            charList = charList.OrderByDescending(c => c).ToArray(); //<--- I don't get it. What is keySelector?
+            for (int i = 0; i <= charList.Length-1; i++)
+            {
+                char c = (char)charList[i];
+                result += c;
+            }
+            return result;
+        }
+    }
+}
