2adacb97e9c18f37589005cc986d989e3579d94c
Tobias R. <no@thankyou.com>
2026-01-13 09:20:56 +0100

    Add project files.

---
+++ DTP_Strings_Add_seperator_ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DTP_Strings_Add_seperator_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "STRING";
+            Console.WriteLine(Method(str));
+        }
+        static string Method(string str)
+        {
+            string newString = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                newString += str[i] + "_";
+            }
+            return newString.Remove(newString.LastIndexOf("_"));
+        }
+    }
+}
